763228f77b27f5169eed224c7e5d42c62311c247
HP <HP@DESKTOP-7MQ5AUD>
2022-01-16 12:34:42 +0530

    Non-assisted Programs

---
+++ Non-assistedPrograms/src/nonAssistedPrograms/ValidateEmailid.java
@@ -0,0 +1,47 @@
+package nonAssistedPrograms;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateEmailid {
+
+	public static void main(String[] args) {
+		
+		boolean flag=false;
+		
+		ArrayList<String> emailId=new ArrayList<String>();
+		
+		System.out.println("Here the list of email-Id's: ");
+		System.out.println();
+		
+		
+		emailId.add("abcdef@gmail.com");
+		emailId.add("ghijkl@hotmail.com"); 
+		emailId.add("mnopqr@hotmail.com");
+		
+	    System.out.println(emailId);
+	    System.out.println();
+	    
+	    Scanner scan=new Scanner(System.in);
+	    
+	    System.out.println("enter the email-id to search: ");
+	    System.out.println();
+	    
+	    String check=scan.nextLine();
+	    
+	    for (String element : emailId){
+		    if (element.contains(check)){
+		    	flag=true;
+		        System.out.println("found "+element);	          
+		    }		     
+		 }
+	    
+	}
+
+}
+
+
+
+
